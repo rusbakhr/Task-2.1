ec858121f017edd8173a0872a9c64b8a57fc9182
Rustam <--->
2023-06-19 21:38:33 +0500

    Initial Commit

---
+++ Task 2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 2.1
+//
+//  Created by Rustam Bakhritdinov on 19.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
